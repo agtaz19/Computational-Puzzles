b79af536cac65917b2387c33e0724aaf3e0d3af8
Alexandre Tilly <agtaz19@gmail.com>
2026-01-17 21:46:51 -0500

    final

--- 3Brown1Blue/1-Clock.py
+++ 3Brown1Blue/1-Clock.py
@@ -10,16 +10,17 @@ import random as r
 #Parameters
 probability_right = 0.5
 probability_left = 1-probability_right
-number_of_simulations = 1
+number_of_simulations = 1000000
 hour_total_terminated = [0,0,0,0,0,0,0,0,0,0,0,0] #List number of times it ended on each hour
 
 for i in range(0, number_of_simulations):
     visited_every_hour = False #Flag to check if every hour has been visited
     hour_location = 0 #initial location
+    hour_visted_list = [True, False, False, False, False, False, False, False, False, False, False, False] #list of hours visited
 
-
+    print("Simulation number: ", i+1)
     while(not visited_every_hour):
-        hour_visted_list = [True, False, False, False, False, False, False, False, False, False, False, False] #list of hours visited
+
         move_direction = r.random()
 
         if(move_direction < probability_right):
@@ -29,10 +30,13 @@ for i in range(0, number_of_simulations):
         
         hour_visted_list[hour_location] = True
         
-        if(all(hour_visted_list) == True):
+        if(len(set(hour_visted_list)) == 1):
             hour_total_terminated[hour_location] += 1
-            visited_every_hour == True
+            visited_every_hour = True
+        
 
 print("Number of simulations: ", number_of_simulations)
-print("Hour termination counts: ", hour_total_terminated)
-print("Probability of terminating on 6: ", hour_total_terminated[6]/number_of_simulations)
+for i in range(len(hour_total_terminated)): 
+    print("Number of termininations on hour ", i, ": ", "{:,}".format(hour_total_terminated[i]))
+    hour_total_terminated[i] = hour_total_terminated[i]/number_of_simulations
+    print("Percentage of terminations on hour ", i, ": ", "{:.4f}%".format(hour_total_terminated[i]*100))

12852bf77ef1bc4c03233b350caa12797343bb7c
Alexandre Tilly <agtaz19@gmail.com>
2026-02-18 20:32:58 -0500

    add new puzzel

---
+++ 3Brown1Blue/2-GridBacteria.py
@@ -0,0 +1,7 @@
+"""
+Bacteria on the grid,
+The Rule: If the spot above and right are empty it can replicate to those positions and dies in the old position, only one cell is allowed at each point so if it is blocked it can't replicate
+
+Begin with one cell at the origin, what is the minimum amount of moves it takes to clear a 3x3 box (ie (3,3), (3,0), (0,3), (0,0))
+
+"""
